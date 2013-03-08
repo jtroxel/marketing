57c50984e6ee8928f79ef8f2415996857c524cdf
John Troxel <jtroxel@yahoo.com>
2013-03-08 11:29:02 -0700

    New rails app with rspec, bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marketing::Application.config.secret_token = '0822d24983100874c45ed3f0a04e05249c20a050a9b699dd39fbeae68f76d7fed3d16b1d15e217a949f4e261722c58c7d6c4bf69762581a305e775d65bb2b9e7'
